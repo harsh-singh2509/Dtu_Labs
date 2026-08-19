07a77ec73342e1e3caf159a89e70a0bd7455f2f7
Harsh Singh <harshyt250908@gmail.com>
2026-08-19 07:23:35 +0530

    Addition of comment

--- Semester_1/nature.c
+++ Semester_1/nature.c
@@ -4,6 +4,7 @@ int main()
     int num;
     printf("Number:");
     scanf("%d", &num);
+    // There should be two == as not assigning a value but checking for equality. 
     (num % 2 == 0)?printf("%d is even.", num):printf("%d is odd.", num);
     return 0;
 }
